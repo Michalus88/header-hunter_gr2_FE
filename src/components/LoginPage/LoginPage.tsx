25389080b87063398f7fe3a7a46289474e8696b5
xmNuc <xmnuc@o2.pl>
2022-07-21 17:56:19 +0200

    Add Navlinks, change input border color.

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { NavLink } from 'react-router-dom';
 import megaK from '../../assets/img/MegaK.webp';
 
 export const LoginPage = () => {
   const [value1, setValue1] = useState('');
   const [value2, setValue2] = useState('');
+
   return (
     <div className="login-page-login-group">
       <img className="login-group-image" src={megaK} alt="MegaK logo" />
@@ -18,7 +20,9 @@ export const LoginPage = () => {
               value={value1}
               onChange={(e) => setValue1(e.target.value)}
             />
-            <label htmlFor="email">Email</label>
+            <label className="login-input-label" htmlFor="email">
+              E-mail
+            </label>
           </span>
         </div>
         <div className="login-input-password">
@@ -29,13 +33,20 @@ export const LoginPage = () => {
               value={value2}
               onChange={(e) => setValue2(e.target.value)}
             />
-            <label htmlFor="password">Hasło</label>
+            <label className="login-input-label" htmlFor="password">
+              Hasło
+            </label>
           </span>
         </div>
-        <div className="login-forgot-pass">Zapominiałeś hasła?</div>
+        <div className="login-forgot-pass">
+          <NavLink to="no_link_yet">Zapominiałeś hasła?</NavLink>
+        </div>
         <div className="register-new-acc">
           <div className="register-new-acc-text">
-            Nie masz konta? <span className="register-new-acc-register">Zarejestruj się</span>
+            Nie masz konta?
+            <span className="register-new-acc-register">
+              <NavLink to="no_link_yet">Zarejestruj się</NavLink>
+            </span>
           </div>
           <Button className="register-new-acc-button" label="Zaloguj się" />
         </div>
