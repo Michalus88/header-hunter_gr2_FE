596f5d3528c60b316f5543d432040985eedd24f5
xmNuc <xmnuc@o2.pl>
2022-07-21 02:01:53 +0200

    Add Login page, logo megaK

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import megaK from '../../assets/img/MegaK.webp';
 
 export const LoginPage = () => {
-  return <div className="login-page-wrap">Login page</div>;
+  return (
+    <>
+      <div className="login-page-login-group">
+        <img src={megaK} alt="MegaK logo" />
+      </div>
+      ;
+    </>
+  );
 };
