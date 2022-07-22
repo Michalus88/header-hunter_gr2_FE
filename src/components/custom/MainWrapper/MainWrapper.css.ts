b63fe069cb2f090f2147e77ee56449b0881f4f21
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-07-22 09:13:32 +0200

    create ThemeProvider from lib styled-components, create theme with global colors for project  and create MainWrapper

---
+++ src/components/custom/MainWrapper/MainWrapper.css.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.aside`
+  width: 1440px;
+  margin: 100px;
+  background-color: grey;
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/style.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import 'primereact/resources/themes/mdc-dark-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import { theme } from './assets/styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
