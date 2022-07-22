377863814d6bc8e98850297323ac9c28fbd46fef
Michal <83036436+naukagit1@users.noreply.github.com>
2022-07-22 19:21:38 +0200

    Fixed problem with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import {LoginPage} from './components/LoginPage/LoginPage';
-import {Test} from './components/Test/Test';
-import {BookInterview} from "./components/BookInterView/BookInterview";
+import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from './components/LoginPage/LoginPage';
+import { Test } from './components/Test/Test';
+import { BookInterview } from './components/BookInterView/BookInterview';
 
 export const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<LoginPage/>}/>
-            <Route path="/interview" element={<BookInterview/>}/>
-            <Route path="/test" element={<Test/>}/>
-            <Route path="*" element={<LoginPage/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/interview" element={<BookInterview />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
+  );
 };
--- src/components/BookInterView/BookInterview.tsx
+++ src/components/BookInterView/BookInterview.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export const BookInterview = () => {
-    return (
-        <p>test</p>
-    )
-}
+  return <p> test</p>;
+};
