f1bbea83462119dad4c0f6fb5876abd74fbbb7ff
Michal <83036436+naukagit1@users.noreply.github.com>
2022-07-23 15:18:41 +0200

    Change style one student view in bookInterview component

--- src/components/BookInterView/BookInterview.tsx
+++ src/components/BookInterView/BookInterview.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import '../../assets/css/BookInterview.css';
 import { Button } from 'primereact/button';
-import megaK from '../../assets/img/MegaK.webp';
+import '../../assets/css/BookInterview.css';
+import '../../assets/css/style.css';
 
 export const BookInterview = () => {
   return (
     <div className="BookInterview">
       <div className="BookInterview__student">
-        //@TODO kontener dla rezerwacji i informacji użytkownika
-        <div className="BookInterview__student--reservation">
-          <p className="BookInterview__student--reservation-text">Rezerwacja do</p>
-          <p className="BookInterview__student--reservation-date">10.01.2001</p>
-        </div>
-        <div className="BookInterview__student--information">
-          <img
-            className="BookInterview__student--information-avatar"
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            alt="avatar użytkownika"
-          />
-          <p className="BookInterview__student--information"> Michał Testowy</p>
+        <div className="BookInterview__student">
+          <div className="BookInterview__student--reservation">
+            <p className="BookInterview__student--reservation-text">Rezerwacja do</p>
+            <p className="BookInterview__student--reservation-date">10.01.2001</p>
+          </div>
+
+          <div className="BookInterview__student--information">
+            <img
+              className="BookInterview__student--information-avatar"
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+              alt="avatar użytkownika"
+            />
+            <p className="BookInterview__student--information-name"> Michał Testowy</p>
+          </div>
         </div>
         <div className="BookInterview__student--buttons">
           <Button
