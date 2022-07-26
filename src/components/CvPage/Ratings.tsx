e03bd94a38fe186df9593e12d0d21ee3ebf96c05
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-07-26 09:20:37 +0200

    create of a reusable component: Rate, and add css to it

--- src/components/CvPage/Ratings.tsx
+++ src/components/CvPage/Ratings.tsx
@@ -1,43 +1,17 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 import '../../assets/css/cvPage.css';
+import { Rate } from './Rate';
 
-const num = 3;
 export const Ratings = () => {
   return (
     <div className="cv-ratings">
       <h3>Oceny</h3>
       <div className="cv-ratings__container">
-        <div className="cv-ratings__item">
-          <p className="cv-ratings__title">Ocena przejścia kursu</p>
-          <div className="cv-ratings__rating">
-            <span className="cv-ratings__real">5</span> <span className="cv-ratings__max">/5</span>
-            <div className="stars-container">
-              <FontAwesomeIcon
-                icon={faStar}
-                className={`${num >= 1 ? 'stars-container__red-icon' : null}`}
-              />
-              <FontAwesomeIcon
-                icon={faStar}
-                className={`${num >= 2 ? 'stars-container__red-icon' : null}`}
-              />
-              <FontAwesomeIcon
-                icon={faStar}
-                className={`${num >= 3 ? 'stars-container__red-icon' : null}`}
-              />
-              <FontAwesomeIcon
-                icon={faStar}
-                className={`${num >= 4 ? 'stars-container__red-icon' : null}`}
-              />
-              <FontAwesomeIcon
-                icon={faStar}
-                className={`${num >= 5 ? 'stars-container__red-icon' : null}`}
-              />
-            </div>
-          </div>
-        </div>
+        <Rate text="Ocena przejścia kursu" rate={4} />
+        <Rate text="Ocena aktywności i zaangażowania na kursie" rate={2} />
+        <Rate text="Ocena kodu w projekcie własnym" rate={5} />
+        <Rate text="Ocena prcy w zespole Scrum" rate={4} />
       </div>
     </div>
   );
