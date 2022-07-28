a6c2e1a73052baf93bed57a0bdd1cb6f30c132e7
xmNuc <xmnuc@o2.pl>
2022-07-29 00:38:37 +0200

    Add filter. refactor input in search, remove bugs

--- src/components/SearchFiltration/SearchFiltration.tsx
+++ src/components/SearchFiltration/SearchFiltration.tsx
@@ -1,29 +1,50 @@
-import React, {useState} from "react";
+import { InputText } from 'primereact/inputtext';
+import React, { useState } from 'react';
 import magnifer from '../../assets/img/magnifier.png';
+import { MegaButton } from '../Elements/MegaButton';
+import { FilterDialog } from '../FilterDialog/FilterDialog';
 
 export const SearchFiltration = () => {
-    const [search, setSearch] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const [displayFilterDialog, setSisplayFilterDialog] = useState(false);
 
-    const clickSearch = () => {
-        console.log(`Szukam ${search}`);
-    }
-    const clickFilter = () => {
-        console.log('Filtrowanie');
-    }
-    return (
-        <>
-            <div className="search-filtration">
-                <p className="input-wrapper">
-                    <img onClick={clickSearch} src={magnifer} alt=""/>
-                    <input
-                        type="text"
-                        value={search}
-                        placeholder="Szukaj"
-                        onChange={(e) => setSearch(e.target.value)}
-                    />
-                </p>
-                <a href="#" onClick={clickFilter}>Filtrowanie</a>
-            </div>
-        </>
-    );
+  const toggleFilterDialog = () => {
+    setSisplayFilterDialog(!displayFilterDialog);
+  };
+
+  //   const clickSearch = () => {
+  //     console.log(`Szukam ${search}`);
+  //   };
+
+  return (
+    <div className="search-filtration">
+      <p className="input-wrapper">
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search"
+          />
+        </span>
+
+        {/* <button className="search-btn" type="button" onClick={clickSearch}>
+          <img src={magnifer} alt="" />
+        </button>
+
+        <input
+          type="text"
+          value={search}
+          placeholder="Szukaj"
+          onChange={(e) => setSearch(e.target.value)}
+        /> */}
+      </p>
+      <MegaButton
+        classNameAdd="megak-secondary megak-small-height"
+        buttonTitle="Filter"
+        onClick={() => toggleFilterDialog()}
+      />
+      <FilterDialog visible={displayFilterDialog} toggleFilterDialog={toggleFilterDialog} />
+    </div>
+  );
 };
