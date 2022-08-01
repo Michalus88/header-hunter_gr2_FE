38a9336bfc7fa678758526774ff77a58d4b5b0da
xmNuc <xmnuc@o2.pl>
2022-08-01 23:07:04 +0200

    Add lost pass option

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -10,61 +10,91 @@ import { TestBtn4 } from '../Test/TestBtn4';
 import { TestBtn5 } from '../Test/TestBtn5';
 
 export const LoginPage = () => {
+  const [login, setLogin] = useState(true);
   const [value1, setValue1] = useState('');
   const [value2, setValue2] = useState('');
 
-  const clicked = () => {
-    console.log('Clicked');
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log('SUBMIT', value1, value2);
+  };
+  const toggleLostPass = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setLogin(!login);
+    if (login) {
+      setValue2('');
+    }
+    console.log('Zapomniało się...', value1);
   };
 
   return (
     <div className="login-page-login-group">
       <img className="login-group-image" src={megaK} alt="MegaK logo" />
       <div>
-        <div className="login-input-email">
-          <span className="p-float-label">
-            <InputText
-              id="email"
-              size={40}
-              value={value1}
-              onChange={(e) => setValue1(e.target.value)}
-            />
-            <label className="login-input-label" htmlFor="email">
-              E-mail
-            </label>
-          </span>
-        </div>
-        <div className="login-input-password">
-          <span className="p-float-label">
-            <Password
-              id="password"
-              size={40}
-              value={value2}
-              onChange={(e) => setValue2(e.target.value)}
-              promptLabel="Wpisz hasło"
-              feedback={false}
-            />
-            <label className="login-input-label" htmlFor="password">
-              Hasło
-            </label>
-          </span>
-        </div>
-        <div className="login-forgot-pass">
-          <NavLink to="no_link_yet">Zapominiałeś hasła?</NavLink>
-        </div>
-        <div className="register-new-acc">
-          <div className="register-new-acc-text">
-            Nie masz konta?
-            <span className="register-new-acc-register">
-              <NavLink to="no_link_yet">Zarejestruj się</NavLink>
+        <form className="sign-form row  justify-content-center mt-5" onSubmit={handleSubmit}>
+          <div className="login-input-email">
+            <span className="p-float-label">
+              <InputText
+                id="email"
+                size={40}
+                value={value1}
+                onChange={(e) => setValue1(e.target.value)}
+              />
+              <label className="login-input-label" htmlFor="email">
+                E-mail
+              </label>
             </span>
           </div>
-          <MegaButton
-            classNameAdd="register-new-acc-button"
-            buttonTitle="Zaloguj się"
-            onClick={() => clicked()}
-          />
-        </div>
+          {login ? (
+            <div className="login-input-password">
+              <span className="p-float-label">
+                <Password
+                  id="password"
+                  size={40}
+                  value={value2}
+                  onChange={(e) => setValue2(e.target.value)}
+                  promptLabel="Wpisz hasło"
+                  feedback={false}
+                />
+                <label className="login-input-label" htmlFor="password">
+                  Hasło
+                </label>
+              </span>
+            </div>
+          ) : (
+            <div>Podaj email</div>
+          )}
+          <div className="login-forgot-pass">
+            {login ? (
+              <NavLink to="#" onClick={toggleLostPass}>
+                Zapominiałeś hasła?
+              </NavLink>
+            ) : (
+              <NavLink to="#" onClick={toggleLostPass}>
+                Zaloguj się
+              </NavLink>
+            )}
+          </div>
+          <div className="register-new-acc">
+            <div className="register-new-acc-text">
+              Nie masz konta?
+              <span className="register-new-acc-register">
+                <NavLink className="register" to="no_link_yet">
+                  Zarejestruj się
+                </NavLink>
+              </span>
+            </div>
+            {login ? (
+              <button type="submit" className="register-new-acc-button mega-k-button">
+                Zaloguj się
+              </button>
+            ) : (
+              <button type="submit" className="register-new-acc-button mega-k-button">
+                Wyślij email
+              </button>
+            )}
+          </div>
+        </form>
       </div>
       <div
         style={{
