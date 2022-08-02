5d58e51aff26dab39572be96c92d1d877637ae0f
Jakub Król <jakubkrol@o2.pl>
2022-08-02 15:46:21 +0200

    Empty

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override } = require("customize-cra");
-const {
-    aliasDangerous,
-    configPaths,
-} = require("react-app-rewire-alias/lib/aliasDangerous");
-
-module.exports = {
-    webpack: override(aliasDangerous(configPaths("./tsconfig.paths.json"))),
-};
