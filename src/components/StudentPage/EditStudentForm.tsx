c7b215cf8a72ea7ce407115af0f604da115d4d14
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-08-02 17:24:51 +0200

    fixed eslint and tsconfig to import from types, add further validated form elements

--- src/components/StudentPage/EditStudentForm.tsx
+++ src/components/StudentPage/EditStudentForm.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-// import { ExpectedTypeWork } from 'types';
+import { ExpectedTypeWork, StudentProfileUpdate, ExpectedContractType } from 'types';
 // import { StudentProfileUpdate } from 'types';
 import { ValidateMsg } from './ValidateMsg';
 
-enum ExpectedTypeWork {
-  AT_LOCATION,
-  READY_TO_MOVE,
-  REMOTE,
-  HYBRID,
-  IRRELEVANT,
-}
+// enum ExpectedTypeWork {
+//   AT_LOCATION,
+//   READY_TO_MOVE,
+//   REMOTE,
+//   HYBRID,
+//   IRRELEVANT,
+// }
 
-enum ExpectedContractType {
-  EMPLOYMENT_CONTRACT,
-  B_TO_B,
-  COMMISSION_CONTRACT_OR_SPECIFIC_TASK_CONTRACT,
+// enum ExpectedContractType {
+//   EMPLOYMENT_CONTRACT,
+//   B_TO_B,
+//   COMMISSION_CONTRACT_OR_SPECIFIC_TASK_CONTRACT,
+// }
+
+interface StudentProfileWithArrayUrls extends StudentProfileUpdate {
+  portfolio1: string | undefined;
+  portfolio2: string | undefined;
+  portfolio3: string | undefined;
+  portfolio4: string | undefined;
+  portfolio5: string | undefined;
 }
 
 interface StudentProfileRegister {
@@ -39,19 +47,19 @@ interface StudentProfileRegister {
 }
 
 export const EditStudentForm = () => {
-  const [dataStudent, setDataStudent] = useState<StudentProfileRegister>({
+  const [dataStudent, setDataStudent] = useState<StudentProfileUpdate>({
     email: 'gnys1001@gmail.com',
     tel: '60000000',
     firstName: 'Sławek',
     lastName: 'Gnyś',
     githubUsername: 'sgnys',
-    portfolioUrls: ['http'],
-    projectUrls: ['url1'],
+    portfolioUrls: ['portfolio1', 'portfolio2'],
+    projectUrls: ['project1', 'project2'],
     bio: 'Lorem ut etiam sit amet nisl purus in mollis nunc sed id semper risus in hendrerit gravida rutrum quisque non tellus',
     expectedTypeWork: ExpectedTypeWork.IRRELEVANT,
     targetWorkCity: 'Łódź',
     expectedContractType: ExpectedContractType.EMPLOYMENT_CONTRACT,
-    expectedSalary: '5 000 zł',
+    expectedSalary: '5 000',
     canTakeApprenticeship: false,
     monthsOfCommercialExp: 0,
     education: 'edukacja',
@@ -64,22 +72,47 @@ export const EditStudentForm = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<StudentProfileRegister>({
+  } = useForm<StudentProfileWithArrayUrls>({
     defaultValues: {
       firstName: dataStudent.firstName,
       lastName: dataStudent.lastName,
       tel: dataStudent.tel,
       githubUsername: dataStudent.githubUsername,
       portfolioUrls: dataStudent.portfolioUrls,
+      projectUrls: dataStudent.projectUrls,
       bio: dataStudent.bio,
       expectedTypeWork: dataStudent.expectedTypeWork,
       email: dataStudent.email,
+      targetWorkCity: dataStudent.targetWorkCity,
+      expectedContractType: dataStudent.expectedContractType,
+      expectedSalary: dataStudent.expectedSalary,
+      canTakeApprenticeship: dataStudent.canTakeApprenticeship,
+      monthsOfCommercialExp: dataStudent.monthsOfCommercialExp,
+      education: dataStudent.education,
+      workExperience: dataStudent.workExperience,
+      courses: dataStudent.courses,
     },
     mode: 'onChange',
   });
 
-  const onSubmit: SubmitHandler<StudentProfileRegister> = (data) => {
-    alert(JSON.stringify(data));
+  if (dataStudent.portfolioUrls) {
+    const [portfolio1, portfolio2, portfolio3, portfolio4, portfolio5] = dataStudent.portfolioUrls;
+    console.log(portfolio1, portfolio2, portfolio3, portfolio4, portfolio5);
+  }
+
+  const potfrolio = watch('portfolioUrls');
+  console.log(potfrolio);
+
+  const canTakeApprenticeshipFromForm = String(watch('canTakeApprenticeship')) !== 'No';
+  console.log('log', canTakeApprenticeshipFromForm);
+
+  const onSubmit: SubmitHandler<StudentProfileUpdate> = (data) => {
+    console.log(data.canTakeApprenticeship);
+    const fff = {
+      ...data,
+      canTakeApprenticeship: canTakeApprenticeshipFromForm,
+    };
+    alert(JSON.stringify(fff));
   };
 
   const names = watch(['firstName', 'lastName']);
@@ -161,6 +194,71 @@ export const EditStudentForm = () => {
           <option value={ExpectedTypeWork.HYBRID}>Hybrydowo</option>
           <option value={ExpectedTypeWork.IRRELEVANT}>Bez znaczenia</option>
         </select>
+        <input
+          type="text"
+          {...register('targetWorkCity', {
+            maxLength: {
+              value: 30,
+              message: 'Max length is 30',
+            },
+          })}
+        />
+        <ValidateMsg text={errors.targetWorkCity && errors.targetWorkCity.message} />
+        <select {...register('expectedContractType')}>
+          <option value={ExpectedContractType.EMPLOYMENT_CONTRACT}>Tylko UoP</option>
+          <option value={ExpectedContractType.B_TO_B}>Możliwe B2B</option>
+          <option value={ExpectedContractType.COMMISSION_CONTRACT_OR_SPECIFIC_TASK_CONTRACT}>
+            Możliwe UZ/UoD
+          </option>
+        </select>
+        <input
+          type="text"
+          {...register('expectedSalary', {
+            maxLength: {
+              value: 5,
+              message: 'Max length 5',
+            },
+          })}
+        />
+        <ValidateMsg text={errors.expectedSalary && errors.expectedSalary.message} />
+        <label>
+          <div>
+            <input
+              type="radio"
+              value="Yes"
+              {...register('canTakeApprenticeship', {
+                required: 'one of this checkbox is required',
+              })}
+            />
+            Tak
+          </div>
+        </label>
+        <label>
+          <div>
+            <input type="radio" value="No" {...register('canTakeApprenticeship')} />
+            Nie
+          </div>
+        </label>
+        <ValidateMsg text={errors.canTakeApprenticeship && errors.canTakeApprenticeship.message} />
+        <input
+          type="number"
+          {...register('monthsOfCommercialExp', {
+            max: {
+              value: 999,
+              message: 'Max length is 999',
+            },
+          })}
+        />
+        <ValidateMsg text={errors.monthsOfCommercialExp && errors.monthsOfCommercialExp.message} />
+        <input type="text" {...register('education')} />
+        <input type="text" {...register('workExperience')} />
+        <input type="text" {...register('courses')} />
+
+        <div>
+          <input type="url" {...register('portfolioUrls')} />
+          <input type="url" {...register('portfolioUrls')} />
+        </div>
+
         <button type="submit">Edytuj</button>
       </div>
     </form>
