9cd73bfd352c01248966394ff2b84e2e0738c3da
Lenovo <piwonskipiotrp>
2022-08-03 20:58:15 +0200

    combined to the same standard components AvailableStudents and BookInterveiw

--- src/components/AvailableStudents/AvailableStudents.tsx
+++ src/components/AvailableStudents/AvailableStudents.tsx
@@ -4,19 +4,37 @@ import { ViewSupport } from '../ViewSupport/ViewSupport';
 import { AvailableOneStudent } from '../AvailableOneStudent/AvailableOneStudent';
 import { TopPanel } from '../TopPanel/TopPanel';
 import { ViewPanel } from '../ViewPanel/ViewPanel';
+import { SimulatedData } from '../SimulatedData/SimulatedData';
 
 export const AvailableStudents = () => {
+    const data = SimulatedData;
+
+    const students = SimulatedData.map((student) => (
+        <AvailableOneStudent
+            key={student.id}
+            firstName={student.firstName}
+            lastName={student.lastName}
+            courseCompletion={student.courseCompletion}
+            courseEngagement={student.courseEngagement}
+            projectDegree={student.projectDegree}
+            teamProjectDegree={student.projectDegree}
+            expectedTypeWork={student.expectedTypeWork}
+            targetWorkCity={student.targetWorkCity}
+            expectedContractType={student.expectedContractType}
+            expectedSalary={student.expectedSalary}
+            canTakeApprenticeship={student.canTakeApprenticeship}
+            workExperience={student.workExperience}
+        />
+    ));
+
   return (
     <>
       <TopPanel />
-        <ViewPanel/>
+      <ViewPanel/>
       <div className="available-students-wrapper">
         <SearchFiltration />
         <div className="students-list">
-          <AvailableOneStudent />
-          <AvailableOneStudent />
-          <AvailableOneStudent />
-          <AvailableOneStudent />
+          {students}
         </div>
         <ViewSupport />
       </div>
