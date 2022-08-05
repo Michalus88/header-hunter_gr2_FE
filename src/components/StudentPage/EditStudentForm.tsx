ad28e6d81b5eca6188ff3a7a457eda1e95d7047d
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-08-05 19:09:58 +0200

    fix returned data in edit form

--- src/components/StudentPage/EditStudentForm.tsx
+++ src/components/StudentPage/EditStudentForm.tsx
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { ExpectedTypeWork, StudentProfileUpdate, ExpectedContractType } from 'types';
-// import { StudentProfileUpdate } from 'types';
 import { ValidateMsg } from './ValidateMsg';
-
-// enum ExpectedTypeWork {
-//   AT_LOCATION,
-//   READY_TO_MOVE,
-//   REMOTE,
-//   HYBRID,
-//   IRRELEVANT,
-// }
-
-// enum ExpectedContractType {
-//   EMPLOYMENT_CONTRACT,
-//   B_TO_B,
-//   COMMISSION_CONTRACT_OR_SPECIFIC_TASK_CONTRACT,
-// }
 
 interface StudentProfileWithArrayUrls extends StudentProfileUpdate {
   portfolio1: string | undefined;
@@ -31,25 +16,25 @@ interface StudentProfileWithArrayUrls extends StudentProfileUpdate {
   project5: string | undefined;
 }
 
-interface StudentProfileRegister {
-  email: string;
-  tel: string | undefined;
-  firstName: string;
-  lastName: string;
-  githubUsername: string;
-  portfolioUrls: string[] | undefined;
-  projectUrls: string[];
-  bio: string | undefined;
-  expectedTypeWork: ExpectedTypeWork;
-  targetWorkCity: string | undefined;
-  expectedContractType: ExpectedContractType;
-  expectedSalary: string | undefined;
-  canTakeApprenticeship: boolean;
-  monthsOfCommercialExp: number;
-  education: string | undefined;
-  workExperience: string | undefined;
-  courses: string | undefined;
-}
+// interface StudentProfileRegister {
+//   email: string;
+//   tel: string | undefined;
+//   firstName: string;
+//   lastName: string;
+//   githubUsername: string;
+//   portfolioUrls: string[] | null;
+//   projectUrls: string[];
+//   bio: string | undefined;
+//   expectedTypeWork: ExpectedTypeWork;
+//   targetWorkCity: string | undefined;
+//   expectedContractType: ExpectedContractType;
+//   expectedSalary: string | undefined;
+//   canTakeApprenticeship: boolean;
+//   monthsOfCommercialExp: number;
+//   education: string | undefined;
+//   workExperience: string | undefined;
+//   courses: string | undefined;
+// }
 
 export const EditStudentForm = () => {
   const [dataStudent, setDataStudent] = useState({
@@ -83,8 +68,8 @@ export const EditStudentForm = () => {
       lastName: dataStudent.lastName,
       tel: dataStudent.tel,
       githubUsername: dataStudent.githubUsername,
-      // portfolioUrls: dataStudent.portfolioUrls,
-      // projectUrls: dataStudent.projectUrls,
+      portfolioUrls: dataStudent.portfolioUrls,
+      projectUrls: dataStudent.projectUrls,
       bio: dataStudent.bio,
       expectedTypeWork: dataStudent.expectedTypeWork,
       email: dataStudent.email,
@@ -100,57 +85,52 @@ export const EditStudentForm = () => {
     mode: 'onChange',
   });
 
-  const portfolios = watch(['portfolio1', 'portfolio2', 'portfolio3', 'portfolio4', 'portfolio5']);
-  const projects = watch(['project1', 'project2', 'project3', 'project4', 'project5']);
-
-  const expected =
+  const tel = String(watch('tel')) === '' ? null : watch('tel');
+  const bio = String(watch('bio')) === '' ? null : watch('bio');
+  const targetWorkCity = String(watch('targetWorkCity')) === '' ? null : watch('targetWorkCity');
+  const expectedSalary = String(watch('expectedSalary')) === '' ? null : watch('expectedSalary');
+  const education = String(watch('education')) === '' ? null : watch('education');
+  const workExperience = String(watch('workExperience')) === '' ? null : watch('workExperience');
+  const courses = String(watch('courses')) === '' ? null : watch('courses');
+  const expectedContractType =
     String(watch('expectedContractType')) === '' ? null : watch('expectedContractType');
-  console.log(expected);
+  const canTakeApprenticeship = String(watch('canTakeApprenticeship')) !== 'No';
 
-  const portfolioArr = portfolios.map((el) => {
-    if (el !== '') {
-      return el;
-    }
-    return null;
-  });
-  const projectArr = projects.map((el) => {
-    if (el !== '') {
-      return el;
-    }
-    return null;
-  });
+  let portfolioArr: (string | undefined | null)[] = [];
+  let projectArr: (string | undefined | null)[] = [];
+  useEffect(() => {
+    const portfolios = watch([
+      'portfolio1',
+      'portfolio2',
+      'portfolio3',
+      'portfolio4',
+      'portfolio5',
+    ]);
+    const projects = watch(['project1', 'project2', 'project3', 'project4', 'project5']);
 
-  // const names = watch(['firstName', 'lastName']);
-  // const [firstName, lastName] = names;
-  // console.log(lastName);
-  // console.log('imiona', names);
-  //
-  // const potfrolio = watch('portfolioUrls');
-  // console.log(potfrolio);
-
-  const canTakeApprenticeshipFromForm = String(watch('canTakeApprenticeship')) !== 'No';
-  console.log('log', canTakeApprenticeshipFromForm);
+    portfolioArr = portfolios.map((el) => (el !== '' ? el : null));
+    projectArr = projects.map((el) => (el !== '' ? el : null));
+  }, [portfolioArr, projectArr]);
 
   const onSubmit: SubmitHandler<StudentProfileUpdate> = (data) => {
     const fff = {
       email: data.email,
-      tel: data.tel,
+      tel,
       firstName: data.firstName,
       lastName: data.lastName,
       githubUsername: data.githubUsername,
       portfolioUrls: portfolioArr,
       projectUrls: projectArr,
-      bio: data.bio,
+      bio,
       expectedTypeWork: data.expectedTypeWork,
-      targetWorkCity: data.targetWorkCity,
-      expectedContractType: data.expectedContractType,
-      expectedSalary: data.expectedSalary,
-      canTakeApprenticeship: canTakeApprenticeshipFromForm,
-      // canTakeApprenticeship: false,
-      monthsOfCommercialExp: 0,
-      education: data.education,
-      workExperience: data.workExperience,
-      courses: data.courses,
+      targetWorkCity,
+      expectedContractType,
+      expectedSalary,
+      canTakeApprenticeship,
+      monthsOfCommercialExp: Number(data.monthsOfCommercialExp),
+      education,
+      workExperience,
+      courses,
     };
     alert(JSON.stringify(fff));
   };
@@ -158,108 +138,177 @@ export const EditStudentForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="student-page__form">
-        <input
-          type="email"
-          {...register('email', {
-            required: 'this is required',
-            pattern: {
-              value: /@/,
-              message: 'Invalid email address',
-            },
-          })}
-        />
+        <div>
+          <label>
+            E-mail:
+            <input
+              className="student-page__input"
+              type="email"
+              {...register('email', {
+                required: 'this is required',
+                pattern: {
+                  value: /@/,
+                  message: 'Invalid email address',
+                },
+              })}
+            />
+          </label>
+        </div>
         <ValidateMsg text={errors.email?.message} />
-        <input
-          type="text"
-          {...register('firstName', {
-            required: 'this is a required',
-            maxLength: {
-              value: 255,
-              message: 'Max length is 255',
-            },
-            minLength: {
-              value: 3,
-              message: 'Min length is 3',
-            },
-          })}
-        />
+
+        <div>
+          <label>
+            Imię:
+            <input
+              className="student-page__input"
+              type="text"
+              {...register('firstName', {
+                required: 'this is a required',
+                maxLength: {
+                  value: 255,
+                  message: 'Max length is 255',
+                },
+                minLength: {
+                  value: 3,
+                  message: 'Min length is 3',
+                },
+              })}
+            />
+          </label>
+        </div>
         <ValidateMsg text={errors.firstName && errors.firstName.message} />
-        <input
-          type="text"
-          {...register('lastName', {
-            required: 'this is a required',
-            maxLength: {
-              value: 255,
-              message: 'Max length is 255',
-            },
-            minLength: {
-              value: 3,
-              message: 'Min length is 3',
-            },
-          })}
-        />
+
+        <div>
+          <label>
+            Nazwisko:
+            <input
+              className="student-page__input"
+              type="text"
+              {...register('lastName', {
+                required: 'this is a required',
+                maxLength: {
+                  value: 255,
+                  message: 'Max length is 255',
+                },
+                minLength: {
+                  value: 3,
+                  message: 'Min length is 3',
+                },
+              })}
+            />
+          </label>
+        </div>
         <ValidateMsg text={errors.lastName && errors.lastName.message} />
-        <input
-          type="text"
-          {...register('tel', {
-            maxLength: {
-              value: 9,
-              message: 'Max length is 9',
-            },
-          })}
-        />
+
+        <div>
+          <label>
+            Numer telefonu:
+            <input
+              className="student-page__input"
+              type="text"
+              {...register('tel', {
+                maxLength: {
+                  value: 9,
+                  message: 'Max length is 9',
+                },
+              })}
+            />
+          </label>
+        </div>
         <ValidateMsg text={errors.tel && errors.tel.message} />
-        <input
-          type="text"
-          {...register('githubUsername', {
-            maxLength: {
-              value: 255,
-              message: 'Max length is 255',
-            },
-          })}
-        />
+
+        <div>
+          <label>
+            GitHub (username):
+            <input
+              className="student-page__input"
+              type="text"
+              {...register('githubUsername', {
+                maxLength: {
+                  value: 255,
+                  message: 'Max length is 255',
+                },
+              })}
+            />
+          </label>
+        </div>
         {/* ToDO -check that user exist at github
         https://github.com/shinnn/gh-account-exists */}
         <ValidateMsg text={errors.githubUsername && errors.githubUsername.message} />
-        <textarea rows={4} cols={50} {...register('bio')} />
-        <select {...register('expectedTypeWork')}>
-          <option value={ExpectedTypeWork.AT_LOCATION}>Na miejscu</option>
-          <option value={ExpectedTypeWork.READY_TO_MOVE}>Gotowość do przeprowadzki</option>
-          <option value={ExpectedTypeWork.REMOTE}>Wyłącznie zdalnie</option>
-          <option value={ExpectedTypeWork.HYBRID}>Hybrydowo</option>
-          <option value={ExpectedTypeWork.IRRELEVANT}>Bez znaczenia</option>
-        </select>
-        <input
-          type="text"
-          {...register('targetWorkCity', {
-            maxLength: {
-              value: 30,
-              message: 'Max length is 30',
-            },
-          })}
-        />
-        <ValidateMsg text={errors.targetWorkCity && errors.targetWorkCity.message} />
-        <select {...register('expectedContractType')}>
-          <option value="">Brak preferencji</option>
-          <option value={ExpectedContractType.EMPLOYMENT_CONTRACT}>Tylko UoP</option>
-          <option value={ExpectedContractType.B_TO_B}>Możliwe B2B</option>
-          <option value={ExpectedContractType.COMMISSION_CONTRACT_OR_SPECIFIC_TASK_CONTRACT}>
-            Możliwe UZ/UoD
-          </option>
-        </select>
-        <input
-          type="text"
-          {...register('expectedSalary', {
-            maxLength: {
-              value: 5,
-              message: 'Max length 5',
-            },
-          })}
-        />
-        <ValidateMsg text={errors.expectedSalary && errors.expectedSalary.message} />
-        <label>
-          <div>
+
+        <div>
+          <label>
+            Krótkie bio:
+            <textarea className="student-page__textarea" rows={4} cols={50} {...register('bio')} />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Preferowane miejsce pracy:
+            <select {...register('expectedTypeWork')}>
+              <option value={ExpectedTypeWork.AT_LOCATION}>Na miejscu</option>
+              <option value={ExpectedTypeWork.READY_TO_MOVE}>Gotowość do przeprowadzki</option>
+              <option value={ExpectedTypeWork.REMOTE}>Wyłącznie zdalnie</option>
+              <option value={ExpectedTypeWork.HYBRID}>Hybrydowo</option>
+              <option value={ExpectedTypeWork.IRRELEVANT}>Bez znaczenia</option>
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Docelowe miasto pracy:
             <input
+              className="student-page__input"
+              type="text"
+              {...register('targetWorkCity', {
+                maxLength: {
+                  value: 30,
+                  message: 'Max length is 30',
+                },
+              })}
+            />
+          </label>
+        </div>
+        <ValidateMsg text={errors.targetWorkCity && errors.targetWorkCity.message} />
+
+        <div>
+          <label>
+            Oczekiwany typ kontraktu:
+            <select {...register('expectedContractType')}>
+              <option value="">Brak preferencji</option>
+              <option value={ExpectedContractType.EMPLOYMENT_CONTRACT}>Tylko UoP</option>
+              <option value={ExpectedContractType.B_TO_B}>Możliwe B2B</option>
+              <option value={ExpectedContractType.COMMISSION_CONTRACT_OR_SPECIFIC_TASK_CONTRACT}>
+                Możliwe UZ/UoD
+              </option>
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Oczekiwane miesięczne netto:
+            <input
+              className="student-page__input"
+              type="text"
+              {...register('expectedSalary', {
+                maxLength: {
+                  value: 5,
+                  message: 'Max length 5',
+                },
+              })}
+            />
+            zł.
+          </label>
+        </div>
+        <ValidateMsg text={errors.expectedSalary && errors.expectedSalary.message} />
+
+        <div>
+          <label>
+            <input
+              className="student-page__radio"
               type="radio"
               value="Yes"
               {...register('canTakeApprenticeship', {
@@ -268,21 +317,25 @@ export const EditStudentForm = () => {
               defaultChecked={dataStudent.canTakeApprenticeship}
             />
             Tak
-          </div>
-        </label>
-        <label>
-          <div>
+          </label>
+        </div>
+
+        <div>
+          <label>
             <input
+              className="student-page__input"
               type="radio"
               value="No"
               {...register('canTakeApprenticeship')}
               defaultChecked={!dataStudent.canTakeApprenticeship}
             />
             Nie
-          </div>
-        </label>
+          </label>
+        </div>
+
         <ValidateMsg text={errors.canTakeApprenticeship && errors.canTakeApprenticeship.message} />
         <input
+          className="student-page__input"
           type="number"
           {...register('monthsOfCommercialExp', {
             max: {
@@ -292,32 +345,47 @@ export const EditStudentForm = () => {
           })}
         />
         <ValidateMsg text={errors.monthsOfCommercialExp && errors.monthsOfCommercialExp.message} />
-        <input type="text" {...register('education')} />
-        <input type="text" {...register('workExperience')} />
-        <input type="text" {...register('courses')} />
+        <textarea
+          className="student-page__textarea"
+          rows={4}
+          cols={50}
+          {...register('education')}
+        />
+        <textarea
+          className="student-page__textarea"
+          rows={4}
+          cols={50}
+          {...register('workExperience')}
+        />
+        <textarea className="student-page__textarea" rows={4} cols={50} {...register('courses')} />
 
         <div>
           <input
+            className="student-page__input"
             type="text"
             {...register('portfolio1')}
             defaultValue={dataStudent.portfolioUrls ? dataStudent.portfolioUrls[0] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('portfolio2')}
             defaultValue={dataStudent.portfolioUrls ? dataStudent.portfolioUrls[1] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('portfolio3')}
             defaultValue={dataStudent.portfolioUrls ? dataStudent.portfolioUrls[2] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('portfolio4')}
             defaultValue={dataStudent.portfolioUrls ? dataStudent.portfolioUrls[3] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('portfolio5')}
             defaultValue={dataStudent.portfolioUrls ? dataStudent.portfolioUrls[4] : ''}
@@ -340,24 +408,28 @@ export const EditStudentForm = () => {
         {/* </div> */}
 
         <div>
-          <input type="text" {...register('project1')} />
+          <input className="student-page__input" type="text" {...register('project1')} />
           <ValidateMsg text={errors.project1 && errors.project1.message} />
           <input
+            className="student-page__input"
             type="text"
             {...register('project2')}
             defaultValue={dataStudent.projectUrls ? dataStudent.projectUrls[1] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('project3')}
             defaultValue={dataStudent.projectUrls ? dataStudent.projectUrls[2] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('project4')}
             defaultValue={dataStudent.projectUrls ? dataStudent.projectUrls[3] : ''}
           />
           <input
+            className="student-page__input"
             type="text"
             {...register('project5')}
             defaultValue={dataStudent.projectUrls ? dataStudent.projectUrls[4] : ''}
