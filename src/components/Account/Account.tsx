df28d780d7fdf25f27f4622fd93661689ba0ea5d
wachcio <wachcio@poczta.fm>
2022-08-10 20:25:47 +0200

    Delete not use imports

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,10 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { EmailChange, PasswordChange } from 'types';
-import { useAuth } from '../../hooks/useAuth';
 import { ChangeEmail } from './ChangeEmail';
 import { ChangePassword } from './ChangePassword';
-import { ValidateMsg } from './ValidateMsg';
 
 export const Account = () => {
   return (
