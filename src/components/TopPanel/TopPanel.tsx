3d2290080a91beac78e261b1da78e146b147b774
Michal Molenda <michalus88@gmail.com>
2022-10-18 13:43:01 +0200

    added redirect to main page

--- src/components/TopPanel/TopPanel.tsx
+++ src/components/TopPanel/TopPanel.tsx
@@ -1,21 +1,36 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router';
+import { LoggedUserRes } from 'types';
 import megaK from '../../assets/img/MegaK.webp';
 import { TopPanelRecruiter } from './TopPanelRecruiter';
 import { TopPanelMenu } from './TopPanelMenu';
+import { useApp } from '../../hooks/useApp';
 
 export const TopPanel = () => {
   const [show, setShow] = useState(false);
   const showMenu = () => {
     setShow(!show);
   };
+  const navigate = useNavigate();
+  const { pathRedirect, user, signOut } = useApp();
+
+  if (user === null) {
+    signOut();
+    return <div />;
+  }
 
   return (
     <div className="top-panel-container">
       <div className="top-panel-wrapper">
-        <NavLink className="top-panel-link" to="/hr/available-students">
+        <div
+          className="top-panel-link"
+          role="button"
+          tabIndex={0}
+          onClick={() => pathRedirect(user)}
+          onKeyDown={() => pathRedirect(user)}
+        >
           <img className="top-panel-logo" src={megaK} alt="MegaK logo" />
-        </NavLink>
+        </div>
         <TopPanelRecruiter showMenu={showMenu} />
         {show && <TopPanelMenu />}
       </div>
